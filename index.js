9c22882b2e7750656e877cf7997e7b385c7aa050
Muralivvrsn <64121609+Muralivvrsn@users.noreply.github.com>
2023-05-15 11:10:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,68 +1,76 @@
 const express = require("express");
-const cors = require("cors");
-const { MongoClient, ObjectId } = require("mongodb");
-const databaseId = 'AlphaTest';
-const collectionId = 'Job';
+const mongoose = require("mongoose");
 
-
-//middleware
 const app = express();
-app.use(cors());
-app.use(express.json());
+const port = 3000;
 
-
-//mongodb url
-const mongoClientURI =
-  "mongodb://forwardappdb:aXWqHXoAq110hcDseuIk32gmeDiefOYwQwELCXYGchRHFvOjWj40wFQ0OHhTcBdTp66idpLoGJ32ACDbjm0auw%3D%3D@forwardappdb.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@forwardappdb@";
-
-
-let collection;
-//function to connect mongodb
-async function connectToMongoDB() {
-  try {
-    const client = new MongoClient(mongoClientURI);
-    await client.connect();
-    console.log("Connected to MongoDB");
-    const database = client.db(databaseId);
-    collection = database.collection(collectionId);
-  } catch (err) {
-    console.error("Error:", err);
-  }
-}
-
-
-
-// Call the function to connect to MongoDB
-connectToMongoDB();
-
-
-
-app.get("/", async (req, res) => {
-  const {skipValue} = req.query;
-  console.log(req.query)
-    try {
-        const documents = await collection.find().skip(parseInt(skipValue)).limit(10).toArray();
-        res.json(documents);
-      } catch (err) {
-        console.error("Error:", err);
-        res.status(500).json({ error: "An error occurred" });
-      }
+// Connect to MongoDB
+mongoose.connect("mongodb://localhost:27017/productsDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB");
 });
 
-app.get("/:id", async(req,res)=>{
-  const {id} = req.params;
-  console.log(id)
-  try{
-    const document = await collection.findOne({_id: new ObjectId(id)});
-    console.log(document)
-    res.json(document)
-  }
-  catch(err){
-    console.log(err)
-    res.json(err)
-  }
-})
 
-app.listen(4000, () => {
-  console.log("Listening on port 4000");
+const productSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+  price: Number,
+});
+const Product = mongoose.model("Product", productSchema);
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get("/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.json(products);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
+
+app.get("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const product = await Product.findById(id);
+    if (!product) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+    res.json(product);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
+
+app.put("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const { price } = req.body;
+  try {
+    const product = await Product.findByIdAndUpdate(
+      id,
+      { price },
+      { new: true }
+    );
+    if (!product) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+    res.json(product);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
+
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
